c313474e5240e69ca96c59a1a4a282f647cd1c19
Ammar Ogeil <95254224+Goldenhawk2003@users.noreply.github.com>
2024-10-29 13:33:19 -0400

    Update Book.js

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,12 +3,21 @@ import FlipPage from 'react-flip-page';
 import './Book.css';
 
 function Book() {
+  // Each page now has its own `leftContent` and `rightContent`
   const pages = [
-    { text: "Welcome to our Tattoo Gallery!", image: "images/page1.jpg" },
-    { text: "Explore different styles and designs", image: "images/page2.jpg" },
-    { text: "Traditional Tattoos", image: "images/page3.jpg" },
-    { text: "Black & Grey Tattoos", image: "images/page4.jpg" },
-    // Add more pages as desired
+    { 
+      leftContent: { text: "Welcome!", image: "images/page1-left.jpg" },
+      rightContent: { text: "Explore our art!", image: "images/page1-right.jpg" }
+    },
+    { 
+      leftContent: { text: "Traditional Tattoos", image: "images/page2-left.jpg" },
+      rightContent: { text: "Black & Grey", image: "images/page2-right.jpg" }
+    },
+    { 
+      leftContent: { text: "Realism Art", image: "images/page3-left.jpg" },
+      rightContent: { text: "Custom Designs", image: "images/page3-right.jpg" }
+    },
+    // Add more pages as needed
   ];
 
   return (
@@ -18,11 +27,19 @@ function Book() {
         uncutPages
         pageBackground="#fafafa"
         animationDuration="800"
+        width={800} // Increase width to make the book bigger
+        height={500} // Increase height to make the book bigger
       >
         {pages.map((page, index) => (
           <article key={index} className="book-page">
-            <img src={page.image} alt={`Page ${index + 1}`} className="page-image" />
-            <p className="page-text">{page.text}</p>
+            <div className="left-page">
+              <img src={page.leftContent.image} alt={`Left Page ${index + 1}`} className="page-image" />
+              <p className="page-text">{page.leftContent.text}</p>
+            </div>
+            <div className="right-page">
+              <img src={page.rightContent.image} alt={`Right Page ${index + 1}`} className="page-image" />
+              <p className="page-text">{page.rightContent.text}</p>
+            </div>
           </article>
         ))}
       </FlipPage>
